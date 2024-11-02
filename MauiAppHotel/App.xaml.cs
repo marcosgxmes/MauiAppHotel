b44d9f182e3a7e1c7988d8c8f21ad793e8399075
Marcos Gomes <129970704+marcosgxmes@users.noreply.github.com>
2024-11-02 17:10:42 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        // Definir dimenções da janela de exibição
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base. CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 700;
+
+            return window; 
+        }
+    }
+}
